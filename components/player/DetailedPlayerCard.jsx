e5e468270d77c37f0e7aa1d7a8c5b1b95aad2f89
koike takayuki <tekasax@gmail.com>
2016-11-06 11:29:32 +0900

    リンクを絶対パスに変更(テスト)

--- components/player/DetailedPlayerCard.jsx
+++ components/player/DetailedPlayerCard.jsx
@@ -17,23 +17,23 @@ export default class DetailedPlayerCard extends React.Component {
           </TableRow>
           <TableRow selectable={false}>
             <TableRowColumn>所属</TableRowColumn>
-            <TableRowColumn><Link to={"team/" + player.current_team.id}>{player.current_team.name}</Link></TableRowColumn>
+            <TableRowColumn><Link to={"/team/" + player.current_team.id}>{player.current_team.name}</Link></TableRowColumn>
           </TableRow>
           <TableRow selectable={false}>
             <TableRowColumn>世代</TableRowColumn>
-            <TableRowColumn><Link to={"player/birth-year/" + player.birth_year}>{player.birth_year}年生まれ</Link></TableRowColumn>
+            <TableRowColumn><Link to={"/player/birth-year/" + player.birth_year}>{player.birth_year}年生まれ</Link></TableRowColumn>
           </TableRow>
           <TableRow selectable={false}>
             <TableRowColumn>中学校</TableRowColumn>
-            <TableRowColumn><Link to={"team/" + player.junior_high_team.id}>{player.junior_high_team.name}</Link></TableRowColumn>
+            <TableRowColumn><Link to={"/team/" + player.junior_high_team.id}>{player.junior_high_team.name}</Link></TableRowColumn>
           </TableRow>
           <TableRow selectable={false}>
             <TableRowColumn>高校</TableRowColumn>
-            <TableRowColumn><Link to={"team/" + player.high_school_team.id}>{player.high_school_team.name}</Link></TableRowColumn>
+            <TableRowColumn><Link to={"/team/" + player.high_school_team.id}>{player.high_school_team.name}</Link></TableRowColumn>
           </TableRow>
           <TableRow selectable={false}>
             <TableRowColumn>大学</TableRowColumn>
-            <TableRowColumn><Link to={"team/" + player.university_team.id}>{player.university_team.name}</Link></TableRowColumn>
+            <TableRowColumn><Link to={"/team/" + player.university_team.id}>{player.university_team.name}</Link></TableRowColumn>
           </TableRow>
         </TableBody>
       </Table>
